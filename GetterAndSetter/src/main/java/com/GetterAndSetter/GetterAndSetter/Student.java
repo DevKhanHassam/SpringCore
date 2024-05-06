f848304045f1bb1c6560b1fc9b1299f586ad7483
Hassam <hassamkhan98@outlook.com>
2024-05-06 19:47:56 +0200

    add constructor

--- GetterAndSetter/src/main/java/com/GetterAndSetter/GetterAndSetter/Student.java
+++ GetterAndSetter/src/main/java/com/GetterAndSetter/GetterAndSetter/Student.java
@@ -5,7 +5,14 @@ public class Student {
 	private String StudentName;
 	private int StudentId;
 	
+	private String Studenthobby;
 	
+	
+	public Student(String Studenthobby) {
+		super();
+		this.Studenthobby = Studenthobby;
+	}
+
 	public void setStudentId(int StudentId)
 	{
 		this.StudentId=StudentId;
@@ -20,6 +27,7 @@ public class Student {
 	public void showDetails()
 	{
 		System.out.println("Student Name is: " +StudentName);
-		System.out.println("Studern Id is: "+StudentId);
+		System.out.println("Studert Id is: "+StudentId);
+		System.out.println("Student hobby is: "+Studenthobby);
 	}
 }
